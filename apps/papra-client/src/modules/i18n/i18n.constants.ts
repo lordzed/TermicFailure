2392d67ec126efd5996d67fc329e172458800b2d
Lordzed <lzzed07@gmail.com>
2025-07-05 00:43:40 +0300

    Added some translations
    Added some translations

--- apps/papra-client/src/modules/i18n/i18n.constants.ts
+++ apps/papra-client/src/modules/i18n/i18n.constants.ts
@@ -2,5 +2,6 @@ export const locales = [
   { key: 'en', name: 'English' },
   { key: 'fr', name: 'Français' },
   { key: 'de', name: 'Deutsch' },
+  { key: 'ro', name: 'Romana' },
   { key: 'pt-BR', name: 'Português Brasileiro' },
 ] as const;
